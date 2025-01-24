321401902b47d33bd8c6f1671616f1a7b1df6c65
Venkat varma <kvenkatvarma@gmail.com>
2025-01-24 11:23:54 +0530

    store example

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'; // Note the change here
-
+import { createStore } from 'redux';
 import App from './App';
 var defaultState =0 ;
 
 //reducers
 const balanceReducer =(state=defaultState,action)=>{
+    console.log("reducer invoked");
     switch (action.type){
         case "DEPOSIT":
             return state + 100;
@@ -16,6 +17,9 @@ const balanceReducer =(state=defaultState,action)=>{
     }
 };
 
+//store
+var store = createStore(balanceReducer);
+console.log(store.getState());
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  
