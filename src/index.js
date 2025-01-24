1d7b69090fc2ee5242ec83a5089aaa389e61d9bf
Venkat varma <kvenkatvarma@gmail.com>
2025-01-24 12:13:31 +0530

    redux DevTools

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client'; // Note the change here
 import { createStore } from 'redux';
 import App from './App';
+import {composeWithDevTools} from "redux-devtools-extension";
 var defaultState =0 ;
 
 //reducers
 const balanceReducer =(state=defaultState,action)=>{
-    console.log("reducer invoked");
+    console.log("reducer invoked",action);
     switch (action.type){
         case "DEPOSIT":
             return state + 100;
@@ -18,8 +19,21 @@ const balanceReducer =(state=defaultState,action)=>{
 };
 
 //store
-var store = createStore(balanceReducer);
+var store = createStore(balanceReducer,composeWithDevTools());
 console.log(store.getState());
+//dispatch
+store.dispatch({
+    type:"DEPOSIT"
+});
+console.log(store.getState());//get the updated state=100
+store.dispatch({
+    type:"DEPOSIT"
+});
+console.log(store.getState());//get the updated state =200
+store.dispatch({
+    type:"WITHDRAW"
+});
+console.log(store.getState());//get the updated state=100
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  
